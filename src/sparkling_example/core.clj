5facbd1052d8b2c13acf5b9a3a43db566b80c947
Paulus Esterhazy <pesterhazy@gmail.com>
2015-01-19 13:37:24 +0100

    Work on examples

--- src/sparkling_example/core.clj
+++ src/sparkling_example/core.clj
@@ -69,9 +69,7 @@
     transform-log-entry))
 
 (defn process-log-entries [in out]
-  (let [
-        lines (s-api/text-file @sc in)
-        ]
+  (let [lines (s-api/text-file @sc in)]
     (-> lines
         (s-api/map parse-line)
         (s-api/filter (fn [entry] (= "200" (:status entry))))
@@ -81,8 +79,7 @@
         (s-api/sort-by-key false)
         (s-api/map-to-pair (s-destructure/key-value-fn (fn [a b] (s-api/tuple b a))))
         (s-api/map (s-destructure/key-value-fn (fn [& xs] (clojure.string/join "\t" xs))))
-        (s-api/save-as-text-file out)
-        )))
+        (s-api/save-as-text-file out))))
 
 ;; call these from the REPL
 ;;
@@ -92,3 +89,36 @@
   ([in] (let [out "output"]
           (sh "rm" "-rf" out)
           (process-log-entries in out)))) 
+
+(defn line-count [lines]
+  (->> lines
+       count))
+
+(defn group-by-status-code [lines]
+  (->> lines
+       (map parse-line)
+       (map (fn [entry] [(:status entry) 1]))
+       (reduce (fn [a [k v]] (update-in a [k] #((fnil + 0) % v))) {})
+       (map identity)))
+
+(defn top-errors [lines]
+  (->> lines
+       (map parse-line)
+       (filter (fn [entry] (not= "200" (:status entry))))
+       (map (fn [entry] [(:uri entry) 1]))
+       (reduce (fn [a [k v]] (update-in a [k] #((fnil + 0) % v))) {})
+       (sort-by val >)
+       (take 10)))
+
+(defn process-log-file [f]
+  (with-open [rdr (clojure.java.io/reader "in.log")]
+    (let [result (f (line-seq rdr))]
+      (if (seq? result)
+        (doall result)
+        result))))
+
+(defn example2 []
+  (let [lines (s-api/text-file @sc "in.log")]
+    (-> lines
+        (s-api/map parse-line)
+        (s-api/count))))
